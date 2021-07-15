e3abea51e19e92ecc2419827d3376305c4749106
Matthias Millette-Paul <matthiasmillette-paul@Matthiass-MacBook-Pro.local>
2021-07-15 14:44:04 -0400

    Saving changes before i mess up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react' // use state allows you to add react state function components. always use at the top of a  react function. 
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import{
+  Route, Link, Switch,Redirect
+} from 'react-router-dom'
 
+import Home from './Home'; // reminds me of object oriented programming. where you split up functions in differnt code files and put them all together in one code file 
+import Lists from './Lists';
 function App() {
 
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([]); // list stores value. set list updates the list
   const [inputData, setInputData] = useState('');
 
   const handleAddItem = () =>{   {/* takes values entered in the text feild amd reads it*/}
@@ -16,14 +22,18 @@ function App() {
 
 const handleRemoveItem = (index) => { {/* removes data once it is clicked */}
   const newList =[];
-  for(let i = 0; i< list.length; i++){ // if value is plus one it will delete?
+  for(let i = 0; i< list.length; i++){ 
     if (index != i){
       newList.push(list[i]);
     }
   }
   setList(newList);
 }
+
   return (
+
+    <Router> {/* keeps UI in sync with the URL */}
+
     <div className='App-header'>
       <h1>My Shopping List</h1>
       <div className='input'>
@@ -42,8 +52,23 @@ const handleRemoveItem = (index) => { {/* removes data once it is clicked */}
                )
              })}
       </div>
-    </div>
 
+    <div className="menu"> {/* allows you to seperate the code and cistomize it according to class name*/}
+      <ul>{/* makes an unordered list*/ }
+        <li><Link to="/"> Home </Link></li> {/* makes a list */}
+       <li> <Link to="/lists"> Lists </Link> {/* renders navigation link */}</li>
+      </ul>
+
+    </div>
+    <div className="App-intro">
+      <Switch> {/* makes path mathcing exclusive rather than inclusive. renders only the first route that matches the location*/}
+      <Route exact path="/"component={Home}/> {/* Route sends the webpage to whatever option the user chooses*/}
+      <Route path="/lists" component={Lists}/>
+      <Redirect to="/"/> {/* navigates to new component if non existent path is entered*/}
+      </Switch>
+    </div>
+    </div>
+     </Router>
   );
 }
 
---
+++ src/Lists.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const List = () => (
+ <div>
+     <h2> Previously made lists </h2>
+     These are your previously made lists
+
+ </div>
+
+);
+
+export default List;
