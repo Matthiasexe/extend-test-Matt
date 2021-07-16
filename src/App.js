ed8dd50c4de33e9163b33ce1fc89c15296cfc83e
Matthias Millette-Paul <matthiasmillette-paul@Matthiass-MacBook-Pro.local>
2021-07-15 22:53:40 -0400

    test 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
 import React, {useState} from 'react' // use state allows you to add react state function components. always use at the top of a  react function. 
-import { render } from '@testing-library/react';
 import { BrowserRouter as Router} from 'react-router-dom';
 import{
   Route, Link, Switch,Redirect
 } from 'react-router-dom'
 
-import Home from './Home'; // reminds me of object oriented programming. where you split up functions in differnt code files and put them all together in one code file 
-import Lists from './Lists';
+import Home from './ShoppingList/Home'; // reminds me of object oriented programming. where you split up functions in differnt code files and put them all together in one code file 
+import Lists from './ShoppingList/Lists';
 function App() {
 
   const [list, setList] = useState([]); // list stores value. set list updates the list
   const [inputData, setInputData] = useState('');
 
-  const handleAddItem = () =>{   {/* takes values entered in the text feild amd reads it*/}
+  const handleAddItem = () =>
+  {   {/* takes values entered in the text feild amd reads it*/}
   const newList = [...list, {title: inputData}];
   setList(newList);
   setInputData('');
   console.log(list)
 }
 
-const handleRemoveItem = (index) => { {/* removes data once it is clicked */}
+const handleRemoveItem = (index) => 
+{ {/* removes data once it is clicked */}
   const newList =[];
   for(let i = 0; i< list.length; i++){ 
     if (index != i){
@@ -30,13 +31,13 @@ const handleRemoveItem = (index) => { {/* removes data once it is clicked */}
   setList(newList);
 }
 
-  return (
+ return (
 
-    <Router> {/* keeps UI in sync with the URL */}
+<Router> {/* keeps UI in sync with the URL */}
 
-<div className='App-header'>
-      <h1>My Shopping List</h1>
-      <div className='input'>
+<div className='background'>
+  <h1>Shopping Helper</h1>
+    <div className='input'>
        <input type='text' value={inputData} onChange={(event) => setInputData(event.target.value)}/>{/* stores values entered into text box. will not save values unless "onChange is used" */}
        
        <input type='button' value="Create Shopping Item" onClick={() => handleAddItem()}/> {/* creates the button. saves datadue to onClick */}
@@ -55,7 +56,7 @@ const handleRemoveItem = (index) => { {/* removes data once it is clicked */}
  
     <div className="menu"> {/* allows you to seperate the code and cistomize it according to class name*/}
       <ul>{/* makes an unordered list*/ }
-        <li><Link to="/"> Home </Link></li> {/* makes a list */}
+       <li><Link to="/"> Home </Link></li> {/* makes a list */}
        <li> <Link to="/lists"> Lists </Link> {/* renders navigation link */}</li>
       </ul>
 
--- src/ShoppingList/Home.js
+++ src/ShoppingList/Home.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Home = () => (
     <div>
-        <h2>Home</h2>
-        Home page
+        
     </div>
 );
 
